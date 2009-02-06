e7a5196dcce7307627ea18bf10e7502d41143fd3
david@elegantcode.com <david@elegantcode.com@3d3df15e-b9a1-11dd-bba1-afcbec1a1e1f>
2009-02-06 04:40:55 +0000

    Initial commit

---
+++ ECTwitterFlair/src/ECTF/Domain/Tweet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ECTF.Domain
+{
+    public class Tweet
+    {
+        public Tweet(string message, TwitterUser user)
+        {
+            Message = message;
+            User = user;
+        }
+
+        public string Message { get; private set; }
+        public TwitterUser User { get; private set; }
+    }
+}
---
+++ ECTwitterFlair/src/ECTF/Page.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Browser;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ECTF
+{
+    public partial class Page : UserControl
+    {
+        public Page()
+        {
+            InitializeComponent();
+
+            LoadXml();
+        }
+
+        private void LoadXml()
+        {
+            WebClient wc = new WebClient();
+            wc.DownloadStringCompleted += new DownloadStringCompletedEventHandler(wc_DownloadStringCompleted);
+            wc.DownloadStringAsync(new Uri("friends_timeline.xml", UriKind.Relative));
+        }
+
+        void wc_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if(e.Error != null)
+            {
+                HtmlPage.Window.Alert(e.Error.ToString());
+                return;
+            }
+
+            XDocument xd = XDocument.Load(new StringReader(e.Result));
+            lbTweets.DataContext = xd;
+        }
+    }
+}
