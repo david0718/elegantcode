15df5136d701bed3f5d6fe2696af1561786ea321
jason@elegantcode.com <jason@elegantcode.com@3d3df15e-b9a1-11dd-bba1-afcbec1a1e1f>
2009-11-21 05:55:29 +0000

    Adding PowershellNUnitSpecificationExtensions and a UnityAutoMocker spike

---
+++ trunk/Coders/JasonJarrett/UnityAutoMocker/src/UnityAutoMocker.1.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Moq.AutoMocking.Testing;
+
+namespace UnityAutoMockerConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine("Starting Tests...");
+				var fixture = new UnityAutoMockContainerFixture();
+				AutoMockContainerFixture.RunAllTests(fixture, Console.WriteLine);
+				Console.WriteLine("Test Complete Tests...");
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.ToString());
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ trunk/Coders/JasonJarrett/UnityAutoMocker/src/UnityAutoMocker.2.0/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnityAutoMocker.VNext")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnityAutoMocker.VNext")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1f416191-3e11-422f-b7a4-eae4ab2d9725")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
