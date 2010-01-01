75ed9b79fe222f2d7c1a3ff6aa61779a08fb1116
jason@elegantcode.com <jason@elegantcode.com@3d3df15e-b9a1-11dd-bba1-afcbec1a1e1f>
2010-01-01 05:57:14 +0000

    Updated snippets of code based on blogged comment feedback.

--- Coders/JasonJarrett/PowershellLoadAssemblyWithoutLockingFile/GetAssemblyVersion.ps1
+++ Coders/JasonJarrett/PowershellLoadAssemblyWithoutLockingFile/GetAssemblyVersion.ps1
@@ -2,14 +2,9 @@
 function get-assembly-version() {
     param([string] $file)
     
-	# load the assembly bytes quickly - as to not lock the file for too long
-	$fileStream = ([System.IO.FileInfo] (Get-Item $file)).OpenRead()
-	$assemblyBytes = new-object byte[] $fileStream.Length
-	$fileStream.Read($assemblyBytes, 0, $fileStream.Length) | Out-Null #out null this because this function should only return the version & this call was outputting some garbage number
-	$fileStream.Close()
-    
-	# return the version of the assembly
-	[System.Reflection.Assembly]::Load($assemblyBytes).GetName().Version;
+	$version = [System.Reflection.AssemblyName]::GetAssemblyName($file).Version;
+
+	$version
 }
 
 $version = get-assembly-version(".\Moq.dll")
