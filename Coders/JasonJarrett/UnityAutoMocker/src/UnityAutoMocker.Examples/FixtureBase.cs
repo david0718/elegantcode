bf6d2a9b2566e992bc3865c97c495775886614d3
jason@elegantcode.com <jason@elegantcode.com@3d3df15e-b9a1-11dd-bba1-afcbec1a1e1f>
2010-01-03 06:48:23 +0000

    Added some examples (from a blog post I'm working on) Updated the registration methods of the container to return the container itself - so you can do a fluent style registration.

---
+++ Coders/JasonJarrett/UnityAutoMocker/src/UnityAutoMocker.Examples/FixtureBase.cs
@@ -0,0 +1,40 @@
+﻿using Moq.AutoMocking;
+using NUnit.Framework;
+
+namespace UnityAutoMocker.Examples
+{
+	public class FixtureBase
+	{
+		private readonly UnityAutoMockContainer _autoMockContainer = new UnityAutoMockContainer();
+
+		protected UnityAutoMockContainer AutoMockContainer
+		{
+			get { return _autoMockContainer; }
+		}
+
+		[TestFixtureSetUp]
+		public void SetupContext_ALL()
+		{
+			Before_all_tests();
+			Because();
+		}
+
+		[TestFixtureTearDown]
+		public void TearDownContext_ALL()
+		{
+			After_all_tests();
+		}
+
+		protected virtual void Before_all_tests()
+		{
+		}
+
+		protected virtual void Because()
+		{
+		}
+
+		protected virtual void After_all_tests()
+		{
+		}
+	}
+}
